2440da598f5181050e244d014336f0ef5171d64f
khalvashi298 <khalvashim2@gmail.com>
2024-11-08 13:10:53 +0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+       int x=35;
+       int y=3;
+
+       int z= x-y;
+
+
+        System.out.println(z);
+
+
+
+    }
+}
