071b9adc4356be8a09d200421ee40dc16289ffe1
khalvashi298 <khalvashim2@gmail.com>
2024-11-08 13:33:58 +0400

    Tester

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,5 +3,13 @@ package org.example;
 public class Test {
     public static void main(String[] args) {
 
+
+        int x=35;
+        int y=3;
+
+        int z= x-y;
+
+
+        System.out.println(z);
     }
 }
